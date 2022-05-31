633e42bb2c214143a51d89554dc83aa3319a75a3
Amira de Moya <demoyaamira@gmail.com>
2022-05-31 00:11:51 -0400

    commit tree 9a3118a980b825777a61165ad287b0f7fbbd6ffc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringif({
+        dogName: "Byron",
+        dogBreed: "Poodle",
+
+    })
+};
+
+fetch("http://localhost:3000/dogs", submitData);
